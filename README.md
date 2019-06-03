00f98ccf177fbb51b2282db218a00ebef6543c91
ylochman <lochman.y@gmail.com>
2019-06-03 21:09:31 +0300

    Update test for module1

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Computer Vision
-* to check the first assignment run `python3 test.py -A 1 -N [desired number of lines]`
-* for the second run `python3 test.py -A 2`
-* etc
+
+## Module 1
+* to check the first assignment run `python3 test_module1.py -A 1 -N [desired number of lines]`
+* to check the second assignment run `python3 test_module1.py -A 2`
+
+## Module 2
--- test_module1.py
+++ test_module1.py
@@ -11,15 +11,21 @@ class Test():
         imagename = os.path.splitext(os.path.split(image_path)[1])[0]
         savedir = os.path.join("res", imagename)
         ht = HoughTransform(image)
-        ht.getSpace(show=False, save=savedir);
-        ht.getLines(N, show=False, save=savedir);
+        ht.getSpace(show=True, save=savedir);
+        ht.getLines(N, show=True, save=savedir);
         np.savetxt(os.path.join(savedir, "lines_{}.txt".format(N)),
                    np.array(ht.lines).T)
         print("Lines (rho, theta):")
         print(np.array(ht.lines).T)
         
-    def assignment2(self):
-        pass
+    def assignment2(self, image_path):
+        image = imread(image_path)
+        imagename = os.path.splitext(os.path.split(image_path)[1])[0]
+        savedir = os.path.join("res", imagename)
+        fast = FAST(image)
+        keypoints = fast.getKeypoints(show=True, save=savedir)
+        brief = BRIEF(image)
+        brief.describe(keypoints)
 
 
 if __name__ == "__main__":
@@ -29,12 +35,13 @@ if __name__ == "__main__":
                         help="# of the assigment",
                         type=int, required=True)
     parser.add_argument("-N", help="number of lines (for the assignment 1)",
-                        type=int, required=True)
+                        type=int)
     args = parser.parse_args()
 
     test = Test()
     if args.assignment == 1:
-        image_path = "./ucu-cv-code/res/marker_cut_rgb_512.png"
+        image_path = "./materials-module1/res/marker_cut_rgb_512.png"
         test.assignment1(image_path, args.N)
     elif args.assignment == 2:
-        test.assignment2()
+        image_path = "./materials-module1/res/marker_cut_rgb_512.png"
+        test.assignment2(image_path)

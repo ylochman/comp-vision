b3cf8fe639c58cf120aa26d3aa0459821467a804
ylochman <lochman.y@gmail.com>
2019-06-24 18:58:16 +0300

    Update text segmentation

--- Segmentation/utils.py
+++ Segmentation/utils.py
@@ -14,16 +14,6 @@ task_dirtree = {
     3: ("object", "obj")
 }
 
-def gray_to_opencv(img, channels=True, uint=True):
-    img = np.array(img)
-    assert len(img.shape) == 2
-    if img.max() <= 1:
-        img = (img * 255).astype(np.uint8)
-    if channels:
-        img = np.stack([img]*3,2)
-    if uint:
-        img = img.astype(np.uint8)
-    return img
 
 def increase_brightness(img, value=20):
     img = img.astype(int)
@@ -43,24 +33,26 @@ def draw_bboxes(image, bboxes):
         image_rect = cv2.rectangle(image_rect, pt1, pt2, (0,0,255), 2)
     return image_rect
 
-def get_example(i, task, channel=None, brightness_value=20, clahe=False, show=True,
+def get_example(i, task, channel=None, brightness_value=20, clahe=False, clahe2=False, show=True,
                 text_bboxes=[]):
     (directory, file) = task_dirtree[task]
     image_path = "materials-module2/tasks/{}/{}{}.jpg".format(directory, file, i)
     image = imread(image_path)
-    if channel is None:
-        image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    else:
-        image_gray = image[:,:,channel]
     
-    if show:
-        imshow(draw_bboxes(image, text_bboxes), sub=(1,3,1))
-#         imshow(image_gray, sub=(1,3,2))
+    image_gray = image.mean(2) if channel is None else image[:,:,channel]
     if clahe:
         clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
         image_gray = clahe.apply(image_gray)
+    if clahe2:
+        l, a, b = cv2.split(cv2.cvtColor(image, cv2.COLOR_RGB2LAB))
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(18,18))
+        l = clahe.apply(l)
+        image_enhanced = cv2.cvtColor(cv2.merge((l,a,b)), cv2.COLOR_LAB2RGB).astype(np.uint8)
+        image_gray = image_enhanced.mean(2) if channel is None else image_enhanced[:,:,channel]
     image_gray = increase_brightness(image_gray, value=brightness_value)
     if show:
+        imshow(draw_bboxes(image, text_bboxes), sub=(1,3,1))
+#         imshow(image_gray, sub=(1,3,2))
         imshow(image_gray, sub=(1,3,2))
     save(image, task, '{}_orig'.format(i))
     return image, image_gray
@@ -78,16 +70,8 @@ def get_mask_from_bboxes(imagesize, bboxes):
         pt2 = bbox[1]
         mask[pt1[0]:pt2[0], pt1[1]:pt2[1]] = 1
     return mask
-    
-def masked_result(text, text_mask):
-    """
-    Args:
-        text: np.array with values 0 (text) or 255 (background)
-        mask: np.array with values 0 (background) or 1 (text)
-    """
-    return 255 - (255 - text) * text_mask
 
-def detect_text(image, kernel=(11,11), dilate_kernel=None, dilate_iter=3, show=False):
+def detect_text(image, image_gray, kernel=(11,11), dilate_kernel=None, dilate_iter=3, show=False):
     if isinstance(kernel, int):
         kernel = (kernel, kernel)
     img_sobel = cv2.Sobel(image_gray, cv2.CV_8U, 1, 0)
@@ -110,8 +94,6 @@ def detect_text(image, kernel=(11,11), dilate_kernel=None, dilate_iter=3, show=F
         imshow(text_mask, sub=(1,2,2))
         plt.show()
     return contours, text_mask[:,:,0]
-
-
 
 def matrix_info(matrix):
     print('Shape: {}'.format(matrix.shape))
